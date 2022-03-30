40e742de97eec30edaffb4948e91bf633d6263d0
Max <67526138+feifyKike@users.noreply.github.com>
2022-03-29 23:07:05 -0400

    Adding extra plot and COVID-19 data.
    Using subplots instead of plot. Creating pointer annotations. Grabbing data for COVID-19 deaths through requests.

--- coronavirus_cases_chart.py
+++ coronavirus_cases_chart.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Cursor
 import requests
 
 link = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
@@ -11,26 +12,76 @@ header = next(csv_data)
 my_list = list(csv_data)
 ask = input("Compare United States Total Cases to ")
 
-dates, c_cases, us_cases = [], [], []
+dates, c_cases, us_cases, us_deaths, c_deaths = [], [], [], [], []
 for row in my_list:
     if row[1] == 'US':
-        us_cases.append(int(row[5]))
         dates.append(row[0])
+        us_cases.append(int(row[5]))
+        us_deaths.append(int(row[7]))
     elif row[2].lower() == ask.lower():
         c_cases.append(int(row[5]))
+        c_deaths.append(int(row[7]))
 
-fig = plt.figure(dpi=130, figsize=(10, 6))
-plt.plot(dates[::30], us_cases[::30], c='red', label="US Cases", marker="^")
-plt.plot(dates[::30], c_cases[::30], c='blue', label=ask + " Cases", marker="s")
+# dpi=130, figsize=(10, 6)
+fig, ax = plt.subplots(2)
+ax[0].plot(dates[::30], us_cases[::30], c='red', label="US Cases", marker="^")
+ax[0].plot(dates[::30], c_cases[::30], c='blue', label=ask + " Cases", marker="s")
+
+ax[1].plot(dates[::30], us_deaths[::30], c='red', label="US Cases", marker="^")
+ax[1].plot(dates[::30], c_deaths[::30], c='blue', label=ask + " Cases", marker="s")
+
+annot = ax[0].annotate(
+    "", 
+    xy=(0,0), 
+    xytext=(-40,40), 
+    textcoords="offset points",
+    bbox=dict(boxstyle='round', fc="white", ec='k', lw=1),
+    arrowprops=dict(arrowstyle='-|>')
+)
+annot2 = ax[1].annotate(
+    "", 
+    xy=(0,0), 
+    xytext=(-40,40), 
+    textcoords="offset points",
+    bbox=dict(boxstyle='round', fc="white", ec='k', lw=1),
+    arrowprops=dict(arrowstyle='-|>')
+)
+annot.set_visible(False)
+annot2.set_visible(False)
+
+def onclick(event):
+    x = event.xdata
+    y = event.ydata
+    formatted = "{:,}".format(y)
+    if event.inaxes == ax[0]:
+        annot.xy = (x, y)
+        annot.set_text(formatted[:formatted.index(".")])
+        annot.set_visible(True)
+    elif event.inaxes == ax[1]:
+        annot2.xy = (x, y)
+        annot2.set_text(formatted[:formatted.index(".")])
+        annot2.set_visible(True)
+    fig.canvas.draw()
+
+fig.canvas.mpl_connect('button_press_event', onclick)
 
 # Format the plot
-plt.title("Total cases in the United States vs. " + ask.title())
-plt.xlabel("Date", fontsize = 14)
-fig.autofmt_xdate()
-plt.ylabel("Total cases (in millions)", fontsize= 14)
-plt.tick_params(axis='both', which='minor', labelsize= 5)
-plt.fill_between(dates[::30], us_cases[::30], c_cases[::30], alpha=0.2)
+fig.set_size_inches(10, 6)
 plt.legend()
-plt.grid(True)
+
+ax[0].set_title("Total cases in the United States vs. " + ask.title())
+ax[1].set_title("COVID-19 Deaths in the United States vs. " + ask.title())
+
+ax[0].set(xlabel="", ylabel="Cases (in 10s of millions)")
+ax[1].set(xlabel="Dates", ylabel="Deaths")
+ax[0].label_outer()
+fig.autofmt_xdate()
+
+ax[0].grid(True)
+ax[1].grid(True)
+
+ax[0].fill_between(dates[::30], us_cases[::30], c_cases[::30], alpha=0.2)
+ax[1].fill_between(dates[::30], us_deaths[::30], c_deaths[::30], alpha=0.2)
+fig.canvas.manager.set_window_title('COVID-19 Dashboard')
 
 plt.show()
